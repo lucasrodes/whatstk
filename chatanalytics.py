0f16b18b03b336be5497ceb152b7ce25b0a3377d
LilKdl <lucasrg@kth.se>
2016-12-05 23:23:52 +0100

    update

--- chatanalytics.py
+++ chatanalytics.py
@@ -8,15 +8,18 @@ def get_names(data):
 	return np.unique(np.array([d[1] for d in data]))
 
 # Obtain dates from conversations from the chat
-def get_dates(data):
-	dates_rep = np.array([d[0][:3] for d in data])
+def get_days(data):
+	days_rep = np.array([d[0][:3] for d in data])
 	# From [1]
-	dates = [list(x) for x in set(tuple(x) for x in dates_rep)]
-	dates = sorted(dates,key=itemgetter(2,1,0))
-	return dates
+	days = [list(x) for x in set(tuple(x) for x in days_rep)]
+	days = sorted(dates,key=itemgetter(2,1,0))
+	return days
 
-# Return DataFrame with interventions of all users (columns) for all dates (rows)
-def get_intervention_table(names, dates, data):
+def get_hours():
+	return ['0'+str(s) if len(str(s)) == 1 else str(s) for s in range(24)]
+
+# Return DataFrame with interventions of all users (columns) for all days (rows)
+def get_intervention_table_days(names, dates, data):
 	# Put dates into nice visual format
 	format_dates = nice_format_dates(dates)
 	interventions_per_day = np.zeros(len(dates))
@@ -35,7 +38,26 @@ def get_intervention_table(names, dates, data):
 
 	return df
 
-	
+# Return DataFrame with interventions of all users (columns) for all hour times (rows)
+def get_intervention_table_hours(names, dates, data):
+	# Put dates into nice visual format
+	format_dates = nice_format_dates(dates)
+	interventions_per_hours = np.zeros(len(dates))
+
+	# Loop for all names
+	df = pd.DataFrame()
+	for name in names:
+		interventions = get_interventions_user(name, data)
+		# Obtain number of interventions per each day contained in dates
+		index = 0
+		for i in range(len(dates)):
+			[interventions_per_day[i], index] = get_number_interventions_per_day(dates[i], 
+				interventions[index:])
+		inter = pd.Series(interventions_per_day, index = format_dates)
+		df.insert(0, name, inter)
+
+	return df
+
 # Parse ['DD','MM','YYYY'] to 'DD/MM/YYYY'
 def nice_format_dates(dates):
 	return [d[0]+"/"+d[1]+"/"+d[2] for d in dates]
--- main.py
+++ main.py
@@ -22,11 +22,16 @@ data = cf.create_samples(lines)
 
 # Obtain the names of the users from the chat
 names = ca.get_names(data)
-# Obtain the dates 
-dates = ca.get_dates(data)
+# Obtain the days 
+days = ca.get_days(data)
+# Obtain the hours
+hours = ca.get_hours()
 
 # Obtain DataFrame containing interventions per user per date
-interventions = ca.get_intervention_table(names, dates, data)
+interventions_days = ca.get_intervention_table_days(names, days, data)
+interventions_hours = ca.get_intervention_table_hours(names, hours, data)
+
+
 
 print "Preparing plots..."
 
@@ -35,26 +40,26 @@ plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
 # Plot scatter plot between all users
-p = pd.tools.plotting.scatter_matrix(interventions, alpha=0.2, figsize=(6, 6), diagonal='kde')
+p = pd.tools.plotting.scatter_matrix(interventions_days, alpha=0.2, figsize=(6, 6), diagonal='kde')
 plt.suptitle(r'Number of interventions per day', fontsize=18)
 plt.show()
 
 # Number of interventions per user
-interventions_per_user = interventions.sum(axis=0)
-interventions_per_user.sort_values(inplace=True, ascending=False)
-interventions_per_user.plot(kind='bar')
+interventions_days_user = interventions_days.sum(axis=0)
+interventions_days_user.sort_values(inplace=True, ascending=False)
+interventions_days_user.plot(kind='bar')
 plt.title(r'Number of interventions per user')
 plt.show()
 
 # Number of interventions per date
-interventions_per_date = interventions.sum(axis=1)
-interventions_per_date.plot(kind='line')
+interventions_days_day = interventions_days.sum(axis=1)
+interventions_days_day.plot(kind='line')
 # Apply Savitzky–Golay filter
-filt_interventions_per_date = signal.savgol_filter(interventions_per_date,15,2)
+filt_interventions_per_date = signal.savgol_filter(interventions_days_day,15,2)
 plt.plot(filt_interventions_per_date,color='red')
 plt.title(r'Number of interventions per day')
 plt.show()
 
-sns.distplot(interventions_per_date,bins=25)
+sns.distplot(interventions_days_day,bins=25)
 plt.title('PDF estimation of the interventiosn per day')
 plt.show()
