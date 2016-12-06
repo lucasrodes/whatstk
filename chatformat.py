9f669b9622a69bf9fc51b93936f223eb6b9f7d51
LilKdl <lucasrg@kth.se>
2016-12-06 01:59:05 +0100

    update structure

--- chatformat.py
+++ chatformat.py
@@ -3,10 +3,13 @@ import sys
 import re
 import numpy as np
 import unicodedata
+from operator import itemgetter
+import pandas as pd
 
 encoding = "utf-8" # or iso-8859-15, or cp1252, or whatever encoding you use
 
-# Returns information in format [date, username and message]
+
+# Maps the input line from string to an array of the form: [[date], username, message]
 def raw2format(l,p):
 	header = l[:p-1]
 
@@ -29,8 +32,9 @@ def raw2format(l,p):
 
 	return [d,n,m]
 
+
 # From raw data to a matrix where each row is in format of raw2format
-def create_samples(lines):
+def clean_data(lines):
 	# Regular expression to find the header of the message
 	pattern = '\d\d.\d\d.\d{,4}, \d\d:\d\d - [^:]*:'
 	p1 = re.compile(pattern)
@@ -40,8 +44,7 @@ def create_samples(lines):
 	for line in lines:
 		m1 = p1.match(line)
 		if (m1 != None):
-			# Pattern found !
-			
+			# Pattern found !		
 			# Remove accents from previous message
 			try:
 				data[-1][2] = remove_accents(data[-1][2])
@@ -69,16 +72,103 @@ def create_samples(lines):
 	
 	return data
 
+
 # Function from [1]
 def remove_accents(byte_string):
 	unicode_string = byte_string.decode(encoding)
 	nfkd_form = unicodedata.normalize('NFKD', unicode_string)
 	return u"".join([c for c in nfkd_form if not unicodedata.combining(c)])
 
+
+# Obtain usernames from the chat
+def get_users(data):
+	return np.unique(np.array([d[1] for d in data]))
+
+
+# Obtain dates from conversations from the chat
+def get_days(data):
+	days_rep = np.array([d[0][:3] for d in data])
+	days = [list(x) for x in set(tuple(x) for x in days_rep)] # From [2]
+	days = sorted(days,key=itemgetter(2,1,0))
+	return days
+
+
+def get_hours():
+	return ['0'+str(s) if len(str(s)) == 1 else str(s) for s in range(24)]
+
+
+# Return DataFrame with interventions of all users (columns) for all days (rows)
+def get_intervention_table_days(users, days, data):
+	# Put dates into nice visual format
+	format_days = nice_format_days(days)
+	interventions_per_day = np.zeros(len(days))
+
+	# Loop for all names
+	df = pd.DataFrame()
+	for user in users:
+		interventions = get_interventions_user(user, data)
+		# Obtain number of interventions per each day contained in dates
+		index = 0
+		for i in range(len(days)):
+			[interventions_per_day[i], index] = get_number_interventions_per_day(days[i], 
+				interventions[index:])
+		inter = pd.Series(interventions_per_day, index = format_days)
+		df.insert(0, user, inter)
+
+	return df
+
+
+# Return DataFrame with interventions of all users (columns) for all hour times (rows)
+def get_intervention_table_hours(users, hours, data):
+	interventions_per_hour = np.zeros(len(hours))
+
+	# Loop for all users
+	df = pd.DataFrame()
+	for user in users:
+		interventions = get_interventions_user(user, data)
+		# Obtain number of interventions per each day contained in hours
+		for i in range(len(hours)):
+			interventions_per_hour[i] = get_number_interventions_per_hour(hours[i], 
+				interventions)
+		inter = pd.Series(interventions_per_hour, index = hours)
+		df.insert(0, user, inter)
+
+	return df
+
+# Parse ['DD','MM','YYYY'] to 'DD/MM/YYYY'
+def nice_format_days(days):
+	return [d[0]+"/"+d[1]+"/"+d[2] for d in days]
+
+
+# Obtain a list with all interventions of username_
+def get_interventions_user(username_, data_):
+	return [d for d in data_ if d[1] == username_]
+
+
+# Return the number of interventions in the given day day_
+# Returns an index referring to the position where next date begins
+# in order to reduce the search in subsequent iterations
+def get_number_interventions_per_day(day_, interv_):
+	s = [1 if i[0][:3] == day_ else 0 for i in interv_]
+	if s[-1] == 0:
+		i = s.index(0)
+	else:
+		i = -1
+	return [sum(s), i]
+
+# Return number of interventions in a specific hour range
+def get_number_interventions_per_hour(hour_, interv_):
+	s = [1 if i[0][3] == hour_ else 0 for i in interv_]
+	return sum(s)
+
+
 # REFERENCES 
 #
 # [1] MiniQuark comment,
 # http://stackoverflow.com/questions/517923/what-is-the-best-way-to-remove-accents-in-a-python-unicode-string
+#
+# [2] Comment from Mark Byers,
+#		http://stackoverflow.com/questions/3724551/python-uniqueness-for-list-of-lists
 
 
 
--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 # -*- coding: utf-8 -*-
 import sys
 import re
-import chatformat as cf
-import chatanalytics as ca
-import unicodedata
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
-import scipy as sp
-from scipy import signal
+import chatformat as cf
+import chatplot as cp
 
 # Read Input, execute $ python main.py < file.in 
 lines = []
@@ -17,49 +13,34 @@ for line in sys.stdin:
 	lines.append(line.rstrip())
 
 # Format the chat text, each row has the format:
-#			 [[day,month,year,hour,minutes], name, message]
-data = cf.create_samples(lines)
+#			 [[day,month,year,hour,minutes], user, message]
+data = cf.clean_data(lines)
+
 
 # Obtain the names of the users from the chat
-names = ca.get_names(data)
-# Obtain the days 
-days = ca.get_days(data)
-# Obtain the hours
-hours = ca.get_hours()
+users = cf.get_users(data)
+# Obtain list of days with interventions
+days = cf.get_days(data)
+# Obtain the hours in a day
+hours = cf.get_hours()
 
-# Obtain DataFrame containing interventions per user per date
-interventions_days = ca.get_intervention_table_days(names, days, data)
-interventions_hours = ca.get_intervention_table_hours(names, hours, data)
+# Obtain DataFrame containing interventions per user per day
+interventions_users_days = cf.get_intervention_table_days(users, days, data)
+# Obtain DataFrame containing interventions per user per hour 
+interventions_users_hours = cf.get_intervention_table_hours(users, hours, data)
 
-
-
+#print interventions_users_hours
 print "Preparing plots..."
 
 # Enable LaTeX fonts
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
 
-# Plot scatter plot between all users
-p = pd.tools.plotting.scatter_matrix(interventions_days, alpha=0.2, figsize=(6, 6), diagonal='kde')
-plt.suptitle(r'Number of interventions per day', fontsize=18)
-plt.show()
+# Plots
+cp.chat_scatter_matrix(interventions_users_days, 'Number of interventions per day')
+cp.plot_total_interventions_users(interventions_users_days)
+cp.plot_interventions_per_day(interventions_users_days)
+cp.plot_distribution_total_interventions_per_day(interventions_users_days)
+cp.plot_interventions_per_hour(interventions_users_hours)
 
-# Number of interventions per user
-interventions_days_user = interventions_days.sum(axis=0)
-interventions_days_user.sort_values(inplace=True, ascending=False)
-interventions_days_user.plot(kind='bar')
-plt.title(r'Number of interventions per user')
-plt.show()
 
-# Number of interventions per date
-interventions_days_day = interventions_days.sum(axis=1)
-interventions_days_day.plot(kind='line')
-# Apply Savitzky–Golay filter
-filt_interventions_per_date = signal.savgol_filter(interventions_days_day,15,2)
-plt.plot(filt_interventions_per_date,color='red')
-plt.title(r'Number of interventions per day')
-plt.show()
-
-sns.distplot(interventions_days_day,bins=25)
-plt.title('PDF estimation of the interventiosn per day')
-plt.show()
