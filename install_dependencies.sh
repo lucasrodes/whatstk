9029450ad9dcfa6145c878fc6da1ed8f6963f48d
lucas rg <lucas@github.io>
2018-01-17 23:19:15 +0900

    modify instal ldependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,12 +4,11 @@ pip3 install --user virtualenv
 
 echo ">> Creating python environment"
 python3  -m venv .whatstk
-source .whatstk/bin/activate
 
 echo ">> Creating virtual environment for Jupyter Notebooks"
 python3 -m ipykernel install --user --name .whatstk --display-name "whatstk"
 
 echo ">> Installing python dependencies"
-pip3 install --user -r requirements.txt
-
+source .whatstk/bin/activate
+pip3 install -r requirements.txt
 
