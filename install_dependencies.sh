6021287e2d71d6ea9576656ea6357cdfa78924f0
lucas rg <lucas@github.io>
2018-01-17 23:28:10 +0900

    update notebooks and installation script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -5,10 +5,12 @@ pip3 install --user virtualenv
 echo ">> Creating python environment"
 python3  -m venv .whatstk
 
+echo ">> Activating venv"
+source .whatstk/bin/activate
+
 echo ">> Creating virtual environment for Jupyter Notebooks"
 python3 -m ipykernel install --user --name .whatstk --display-name "whatstk"
 
 echo ">> Installing python dependencies"
-source .whatstk/bin/activate
 pip3 install -r requirements.txt
 
