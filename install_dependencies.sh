e8647dfc6c602f12741e7e767545c64fa15013da
Lucas Rodés-Guirao <lucasrodes@users.noreply.github.com>
2018-06-01 21:49:33 +0200

    Revert "Prepare repository to be installed as a package via pip"

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,11 +3,13 @@ echo ">> Downloading python virtual environment"
 pip3 install --user virtualenv
 
 echo ">> Creating python environment"
-virtualenv -p python3 .whatstk
-# python3  -m venv .whatstk
+python3  -m venv .whatstk
 
 echo ">> Activating venv"
 source .whatstk/bin/activate
 
 echo ">> Creating virtual environment for Jupyter Notebooks"
 python3 -m ipykernel install --user --name .whatstk --display-name "whatstk"
+
+echo ">> Installing python dependencies"
+pip3 install -r requirements.txt
