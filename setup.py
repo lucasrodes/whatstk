5f97131478a645fb040c98c6f913543f0f109ae2
lucas rg <lucasrg@kth.se>
2019-06-15 19:49:43 +0200

    versioning

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(this_directory, 'requirements.txt'), encoding='utf-8') as
 
 setup(
     name='whatstk',
-    version="0.0.0",
+    version="0.0.1",
     description="Parser and analytics tools for WhatsApp group chats",
     long_description=long_description,
     long_description_content_type='text/markdown',
