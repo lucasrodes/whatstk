0059e5d24c4968192e7275c84a2cc9ffdd2f6577
lucas rg <lucasrg@kth.se>
2019-06-16 00:14:53 +0200

    versioning

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
 """setup script"""
 from setuptools import setup, find_packages
 import os
+import glob
 
 this_directory = os.path.abspath(os.path.dirname(__file__))
-
-with open('README.md') as f:
+with open(os.path.join(this_directory, 'README.md')) as f:
     long_description = f.read()
 
-with open('requirements.txt', encoding='utf-8') as f:
-    requirements = [req.strip() for req in f.readlines()]
-
+with open(os.path.join(this_directory, 'requirements.txt')) as f:
+    requirements = f.readlines()
 
 setup(
     name='whatstk',
-    version="0.1.3",
+    version="0.1.4",
     description="Parser and analytics tools for WhatsApp group chats",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='http://github.com/lucasrodes/whatstk',
     author='Lucas Rodes-Guirao',
     author_email='hi@lcsrg.me',
-    license='MIT',
+    license='GPL-v3',
     install_requires=requirements,
     packages=find_packages('.'),
+    package_dir={'': ','},
+    py_modules=[os.path.splitext(os.path.basename(path))[0] for path in glob.glob('./*.py')],
+    include_package_data=True,
     zip_safe=False,
     classifiers=[
         "Development Status :: 3 - Alpha",
