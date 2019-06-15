3c5e45aa659f7e10ddc93ad662300d0dda479528
lucas rg <lucasrg@kth.se>
2019-06-15 19:41:59 +0200

    setup.py update classifiers

--- setup.py
+++ setup.py
@@ -25,8 +25,9 @@ setup(
     packages=["whatstk"],
     zip_safe=False,
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GPL-3.0 License",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
 )
