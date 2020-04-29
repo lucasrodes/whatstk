c45f989d1d0e8e21e2dcc25e8e48635b61b95be4
lucas rg <lucasrg@kth.se>
2020-04-29 13:28:59 +0200

    backwards compatibility

--- whatstk/alpha/__init__.py
+++ whatstk/alpha/__init__.py
@@ -1,5 +1,12 @@
+"""Will be deprecated"""
+
+
 import sys
+import warnings
 from whatstk.utils import parser, auto_header, exceptions
+
+
+warnings.warn("whatstk.alpha is deprecated, use whatstk.utils instead.", FutureWarning)
 
 # Or simply
 __all__ = [
