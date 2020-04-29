7664037fa85316bf297b28a8813af7015e6a8f1e
lucas rg <lucasrg@kth.se>
2020-04-29 22:09:03 +0200

    script for docs

--- make-docs.sh
+++ make-docs.sh
@@ -1,3 +1,6 @@
+# Make sure to have pydoc-markdown:develop installed
+# pip install git+https://github.com/NiklasRosenstein/pydoc-markdown.git@develop
+
 # Script to make documentation
 mkdir -p docs
 
