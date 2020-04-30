06fa9ebf64e7d199018892b22d98f7767d5a69c3
lucas rg <lucasrg@kth.se>
2020-04-30 19:47:41 +0200

    yaml to yml

--- make-docs.sh
+++ make-docs.sh
@@ -6,7 +6,7 @@ mkdir -p docs
 mkdir -p docs/modules
 
 rm -fr docs/*
-pydoc-markdown pydoc-markdown.yaml -m whatstk >> docs/index.md
+pydoc-markdown pydoc-markdown.yml -m whatstk >> docs/index.md
 
 # mkdir -p docs/modules
 # rm -fr docs/modules/*
