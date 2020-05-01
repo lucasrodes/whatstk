dec6e9b84417f4c58df96d2090d9005e6ef103b8
lucas rg <lucasrg@kth.se>
2020-05-01 18:02:23 +0200

    tests scripts

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -3,15 +3,22 @@ import pandas as pd
 
 
 def test_object_auto():
-    filename = './chats/example.txt'
+    filename = 'tests/chats/example_1.txt'
+    chat = WhatsAppChat.from_txt(filename)
+    assert(isinstance(chat.df, pd.DataFrame))
+
+    filename = 'tests/chats/example_2.txt'
     chat = WhatsAppChat.from_txt(filename)
     assert(isinstance(chat.df, pd.DataFrame))
 
 
 def test_object_hformat():
-    filename = './chats/example.txt'
+    filename = 'tests/chats/example_1.txt'
     hformat = '%d.%m.%y, %H:%M - %name:'
     chat = WhatsAppChat.from_txt(filename)
     assert(isinstance(chat.df, pd.DataFrame))
 
-
+    filename = 'tests/chats/example_2.txt'
+    hformat = '[%y/%m/%d %H:%M] %name:'
+    chat = WhatsAppChat.from_txt(filename)
+    assert(isinstance(chat.df, pd.DataFrame))
