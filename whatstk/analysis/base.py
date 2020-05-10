65e90e5a61b21116c1f0e0b36052980f53d5967c
lucas rg <lucasrg@kth.se>
2020-05-11 01:11:41 +0200

    changin API, now supporting df when chat was the only option

--- whatstk/analysis/base.py
+++ whatstk/analysis/base.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 
-def interventions(chat, date_mode='date', msg_length=False):
+def interventions(df=None, chat=None, date_mode='date', msg_length=False):
     """Get number of interventions per user per unit of time.
 
     The unit of time can be chosen by means of argument `date_mode`.
@@ -10,15 +10,16 @@ def interventions(chat, date_mode='date', msg_length=False):
         Get counts of sent messages per user. Also cumulative.
 
         ```python
-        >>> from whatstk.objects import WhatsAppChat
+        >>> from whatstk import df_from_txt
         >>> from whatstk.analysis interventions
         >>> filename = 'path/to/samplechat.txt'
-        >>> chat = WhatsAppChat.from_txt(filename)
-        >>> counts = interventions(chat, date_mode='date', msg_length=False)
+        >>> df = df_from_txt(filename)
+        >>> counts = interventions(df=df, date_mode='date', msg_length=False)
         >>> counts_cumsum = counts.cumsum()
         ```
         
     Args:
+        df (pd.DataFrame): Chat as DataFrame.
         chat (WhatsAppChat): Object containing parsed WhatsApp chat.
         date_mode (str): Choose mode to group interventions by. Available modes are:
                             - 'date': Grouped by particular date (year, month and day).
@@ -35,17 +36,22 @@ def interventions(chat, date_mode='date', msg_length=False):
         ValueError: if invalid mode is chosen.
 
     """
+    if (df is None) & (chat is None):
+        raise ValueError("Please provide a chat, using either argument `df` or argument `chat`.") 
+    if df is None and chat is not None:
+        df = chat.df
+
     if date_mode == 'date':
-        n_interventions = _interventions(chat, [chat.df.index.date], msg_length)
+        n_interventions = _interventions(df, [df.index.date], msg_length)
         n_interventions.index = pd.to_datetime(n_interventions.index)
     elif date_mode == 'hour':
-        n_interventions = _interventions(chat, [chat.df.index.hour], msg_length)
+        n_interventions = _interventions(df, [df.index.hour], msg_length)
     elif date_mode == 'weekday':
-        n_interventions = _interventions(chat, [chat.df.index.weekday], msg_length)
+        n_interventions = _interventions(df, [df.index.weekday], msg_length)
     elif date_mode == 'hourweekday':
-        n_interventions = _interventions(chat, [chat.df.index.weekday, chat.df.index.hour], msg_length)
+        n_interventions = _interventions(df, [df.index.weekday, df.index.hour], msg_length)
     elif date_mode == 'month':
-        n_interventions = _interventions(chat, [chat.df.index.month], msg_length)
+        n_interventions = _interventions(df, [df.index.month], msg_length)
     else:
         raise ValueError("Mode {} is not implemented. Valid modes are 'date', 'hour', 'weekday', "
                                     "'hourweekday' and 'month".format(date_mode))
@@ -58,22 +64,22 @@ def interventions(chat, date_mode='date', msg_length=False):
     return n_interventions
 
 
-def _interventions(chat, index_date, msg_length):
+def _interventions(df, index_date, msg_length):
     """Get number of interventions per day per user
 
     Args:
-        WhatsAppChat: Chat instance.
+        df (pd.DataFrame): Chat as DataFrame.
 
     Returns: 
         pandas.DataFrame: Table with interventions per day per user.
 
     """
     if msg_length:
-        counts_ = chat.df.copy()
+        counts_ = df.copy()
         counts_['message_length'] = counts_['message'].apply(lambda x: len(x))
         counts = counts_.groupby(by=index_date + ['username']).agg({'message_length': lambda x: x.sum()})
     else:
-        counts = chat.df.groupby(by=index_date + ['username']).agg('count')
+        counts = df.groupby(by=index_date + ['username']).agg('count')
     counts = counts.unstack(fill_value=0)
 
     return counts
