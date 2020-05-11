baeaf3adada2947e768807b31bc0a54ca7916066
lucas rg <lucasrg@kth.se>
2020-05-11 23:53:34 +0200

    command line script updated, now accepts output file name, and specific hformat list

--- whatstk/scripts/generate_chats.py
+++ whatstk/scripts/generate_chats.py
@@ -1,5 +1,7 @@
 """Generate chats in all hformats with `size` number of messages and export them to a given `output_path`."""
 import argparse
+import ast
+from datetime import datetime
 from whatstk.utils.chat_generation import generate_chats_hformats
 
 
@@ -7,9 +9,17 @@ def _parse_args():
     parser = argparse.ArgumentParser()
     # For kubeflow
     # parser.add_argument("--output_path", help="Output path where the data should be saved.")
-    parser.add_argument("output_path", help="Path where to store generated chats. Must exist.", type=str)
+    parser.add_argument("--output-path", help="Path where to store generated chats. Must exist.", 
+                            required=True, type=str)
+    parser.add_argument("--filenames", default=None, help="Filenames. Must be equal length of --hformats.", 
+                            nargs='+')
     parser.add_argument("--size", default=500, help="Number of messages to create per chat. Defaults to 500.", 
                             type=int)
+    parser.add_argument("--hformats", default=None, help="Header format. If None, defaults to all supported hformats."
+                                                         "List formats as 'format 1' 'format 2' ..." , nargs='+')
+    parser.add_argument("--last-timestamp", default=None, help="Timestamp of last message. Format YYYY-mm-dd", 
+                            type=lambda s: datetime.strptime(s, "%Y-%m-%d"))
+    
     parser.add_argument("--verbose", action="store_true", help="Verbosity.")
     args = parser.parse_args()
     return args
@@ -17,4 +27,7 @@ def _parse_args():
 
 def main():
     args = _parse_args()
-    generate_chats_hformats(output_path=args.output_path, size=args.size)
+    generate_chats_hformats(
+        output_path=args.output_path, size=args.size, hformats=args.hformats, last_timestamp=args.last_timestamp,
+        filenames=args.filenames
+    )
