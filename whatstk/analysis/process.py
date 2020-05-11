ed14b809626789723d7ac03f2d9b9f743fd57c48
lucas rg <lucasrg@kth.se>
2020-05-11 23:53:54 +0200

    merging two chats, and renaming users is now possible

---
+++ whatstk/analysis/process.py
@@ -0,0 +1,27 @@
+import pandas as pd
+
+
+def _merge_two_chats(df1, df2):
+    if df1.index.min() <= df2.index.min():
+        df = pd.concat([df1, df2[df2.index > df1.index.max()]])
+    else:
+        df = pd.concat([df2, df1[df1.index > df2.index.max()]])
+    return df
+
+
+def merge_chats(dfs):
+    """Merge several chats into a single one. 
+
+    Can come in handy when you have old exports and new ones, and both have relevant data.
+
+    Args:
+        dfs (list): List with the chats as DataFrames.
+
+    Returns:
+        pandas.DataFrame: Merged chat.
+
+    """
+    df = dfs[0]
+    for i in range(1, len(dfs)):
+        df = _merge_two_chats(df, dfs[i])
+    return df
