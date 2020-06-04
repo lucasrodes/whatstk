e28deeeb0935fd9669406f6034605af49ab92dfd
lucas rg <lucasrg@kth.se>
2020-06-04 18:48:11 +0200

    update prenum

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, lucasrodes'
 author = 'lucasrodes'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0.dev0'
+release = '0.3.0.dev1'
 
 
 # -- General configuration ---------------------------------------------------
