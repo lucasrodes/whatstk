fdec6848c86591437fd32646abf36f8f225ffe83
lucas rg <lucasrg@kth.se>
2021-04-28 22:05:40 +0100

    Bump version: 0.4.1.dev0 → 0.4.2.dev0

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ with open(os.path.join(this_directory, 'requirements.txt')) as f:
 
 setup(
     name='whatstk',
-    version="0.4.1.dev0",
+    version="0.4.2.dev0",
     description="Parser and analytics tools for WhatsApp group chats",
     long_description=long_description,
     long_description_content_type='text/markdown',
