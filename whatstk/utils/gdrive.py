872c4405fbbe80bedb2d2b0e887bab79eb1aecdd
lucas rg <lucasrg@kth.se>
2021-05-23 21:41:06 +0100

    Adding support for G Drive file loading

---
+++ whatstk/utils/gdrive.py
@@ -0,0 +1,81 @@
+import yaml
+from shutil import copyfile
+import os
+
+from pydrive2.auth import GoogleAuth
+from pydrive2.drive import GoogleDrive
+
+
+# Create .config/whatstk/gdrive if it does not exist
+CONFIG_PATH = os.path.join(os.path.expanduser("~"), ".config", "whatstk", "gdrive")
+CLIENT_SECRETS_PATH = os.path.join(CONFIG_PATH, "client_secrets.json")
+SETTINGS_PATH = os.path.join(CONFIG_PATH, "settings.yaml")
+CREDENTIALS_PATH = os.path.join(CONFIG_PATH, "credentials.json")
+
+
+def gdrive_init(client_secret_file):
+    """Initialize GDrive credentials.
+
+    This should only run once before reading a file from Google Drive the first time. Subsequent executions should run seamlessly.
+
+    To obtain `client_secret_file`, follow the instructions from:
+    
+    https://medium.com/analytics-vidhya/how-to-connect-google-drive-to-python-using-pydrive-9681b2a14f20
+
+    Notes:
+
+    - Additionally, make sure to add yourself in Test users, as noted in:
+    https://stackoverflow.com/questions/65980758/pydrive-quickstart-and-error-403-access-denied
+    - Select Desktop App instead of Web Application as the application type.
+
+    Args:
+        client_secret_file (str): Path to clien_secret.json file (Created in Google Console).
+    """
+    if not os.path.isdir(CONFIG_PATH):
+        os.makedirs(CONFIG_PATH, exist_ok=True)
+
+    # Copy credentials to config folder
+    copyfile(client_secret_file, CLIENT_SECRETS_PATH)
+
+    # Create settings.yaml file
+    dix = {
+        "client_config_backend": "file",
+        "client_config_file": CLIENT_SECRETS_PATH,
+        "save_credentials": True,
+        "save_credentials_backend": "file",
+        "save_credentials_file": CREDENTIALS_PATH,
+        "get_refresh_token": True,
+        "oauth_scope": [
+            "https://www.googleapis.com/auth/drive",
+            "https://www.googleapis.com/auth/drive.install",
+        ]
+    }
+    with open(SETTINGS_PATH, 'w') as f:
+        yaml.dump(dix, f)
+
+    # credentials.json
+    gauth = GoogleAuth(settings_file=SETTINGS_PATH)
+    gauth.CommandLineAuth()
+
+
+def _check_gdrive_config():
+    error_msg = (
+        "Google Drive not correctly configured. Run `gdrive_init(client_secret_file)` (from whatstk.utils.gdrive)."
+    )
+    if not os.path.isdir(CONFIG_PATH):
+        raise ValueError(error_msg)
+    for f in [CLIENT_SECRETS_PATH, SETTINGS_PATH]:
+        if not os.path.isfile(f):
+            raise ValueError(error_msg)
+
+
+def _load_str_from_file_id(file_id):
+    _check_gdrive_config()
+    gauth = GoogleAuth(settings_file=SETTINGS_PATH)
+    drive = GoogleDrive(gauth)
+    # Load file using id
+    file_obj = drive.CreateFile({'id': file_id})
+    file_obj.FetchMetadata()
+    # Get raw file content as str
+    txt = file_obj.GetContentString()
+    return txt
