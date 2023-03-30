cbc450bad315d94fe7b7e4180fbc8ba4f5c82302
lucasrodes <lucasrodes@users.noreply.github.com>
2023-03-31 01:19:39 +0200

    lint

--- whatstk/analysis/interventions.py
+++ whatstk/analysis/interventions.py
@@ -5,7 +5,7 @@ from typing import TYPE_CHECKING, List
 import pandas as pd
 from whatstk.utils.utils import COLNAMES_DF, _get_df
 
-if TYPE_CHECKING: # pragma: no cover
+if TYPE_CHECKING:  # pragma: no cover
     from whatstk._chat import BaseChat  # pragma: no cover
 
 
